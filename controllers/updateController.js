fddf477d75f29c32da56eb39628cbf5b3c99c79d
DavidS415 <93840316+DavidS415@users.noreply.github.com>
2022-01-16 23:12:27 -0800

    update user stories

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -3,9 +3,16 @@ const Obrf = require("../models/Obrf");
 //Get request to view update
 
 const updateView = (req, res) => {
-    res.render("update", {
-      user: req.user
-    });
+  Obrf.find({}, function (err, allDetails) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("update", { 
+        details: allDetails,
+        user: req.user
+      });
+    }
+  });
   };
 
 module.exports = {
