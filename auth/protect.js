47e791fc44eaeb34f473a4adacc61f4457e73647
DavidS415 <93840316+DavidS415@users.noreply.github.com>
2022-03-12 21:52:17 -0800

    clean code

--- auth/protect.js
+++ auth/protect.js
@@ -6,13 +6,14 @@ const protectRoute = (req, res, next) =>{
     console.log('Please log in to continue');
     res.redirect('/login');
   }
-  const allowIf = (req, res, next) =>{
+const allowIf = (req, res, next) =>{
     if (!req.isAuthenticated()) {
       return next();
     }
     res.redirect('/dashboard');      
   }
-  module.exports = {
-      protectRoute,
-      allowIf,
+
+module.exports = {
+    protectRoute,
+    allowIf,
     };
